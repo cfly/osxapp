a32ee036238334a9215035fb8ff6bc7f2550c99c
cfly <caofei.org@gmail.com>
2015-02-04 14:16:03 +0800

    0

---
+++ osxapp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  osxapp
+//
+//  Created by cfly on 15/2/4.
+//  Copyright (c) 2015年 org.caofei. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
